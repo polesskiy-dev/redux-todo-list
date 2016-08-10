1cff71cdf8ce1498044c7486ed75f23a4358d7c8
Artem Polisskyi <apoliss@MACMINI304.local>
2016-08-10 13:49:22 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: ['babel-polyfill', './src-frontend/index'],
+    output: {
+        path: path.resolve('./public'),
+        filename: 'js/bundle/bundle.js'
+    },
+    module: {
+        //Validate by ESLint before loading
+        preLoaders: [
+            {
+                test: /\.js$/,
+                loaders: ['eslint']
+            }
+        ],
+        loaders: [
+            //Compile ES6/7 to ES5 via babel
+            {
+                test: /\.js$/,
+                loader: ['babel-loader'],
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'stage-0', 'react']
+                }
+            },
+            //LESS
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        //Extract css to file
+        new ExtractTextPlugin('stylesheets/bundle/bundle.css')
+    ],
+
+    // Pretty terminal output
+    stats: {colors: true},
+
+    // Generate external sourcemaps for the JS & CSS bundles
+    devtool: 'source-map',
+
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
+};
